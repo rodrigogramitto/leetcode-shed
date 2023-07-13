c4b400bf70832d1ff0a7aefdece0feacedac3a26
rodrigogramitto <rgramitto@gmail.com>
2023-07-12 23:06:40 -0400

    working on topKFrequent

---
+++ topKFrequentElements/frequentElements.js
@@ -0,0 +1,38 @@
+// Given an integer array nums and an integer k,
+// return the k most frequent elements.
+// You may return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+// I: num [] , k elements for amount of frequency
+// O: array containing k most frequent elements
+// C: O(n log n) logarithmic time
+// E: N/A
+
+const topKFrequent = function(nums, k) {
+ // define object to hold ocurrences
+ // define result array
+
+ // iterate through nums
+  // if object at nums is undefined
+    // object at nums equals one
+  // increase object at nums by one
+
+
+
+
+// return result array
+};
+
+if (module === undefined) {
+  var module = {}
+}
+
+module.exports = topKFrequent;
