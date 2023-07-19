d2eff9ac52e80c727be2fbe079c57ca2bc8ecb91
rodrigogramitto <rgramitto@gmail.com>
2023-07-19 00:33:00 -0400

    isPalindrome

--- validPalindrome/validPalindrome.js
+++ validPalindrome/validPalindrome.js
@@ -20,7 +20,30 @@
 // Since an empty string reads the same forward and backward, it is a palindrome.
 
 var isPalindrome = function(s) {
+if (s.length <= 1) {
+  return true;
+}
+let [left, right] =  [0, s.length - 1];
+let leftChar, rightChar;
 
+while (left < right) {
+  leftChar = s[left];
+  rightChar = s[right];
+
+  if (!/[a-zA-Z0-9]/.test(leftChar)) {
+    left++;
+  } else if (!/[a-zA-Z0-9]/.test(rightChar)) {
+    right--;
+  } else {
+    if (leftChar.toLowerCase() !== rightChar.toLowerCase()) {
+      return false
+    };
+    left++;
+    right--;
+  }
+}
+return true;
 };
 
-module.export = isPalindrome;
+module.exports = isPalindrome;
+
