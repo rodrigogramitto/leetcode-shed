4ab9673c013cfd681e577b357eac6a5b1d17f8a5
rodrigogramitto <rgramitto@gmail.com>
2023-12-09 17:48:51 -0500

    searchInRotatedSortedArray

--- neetcode75/JavaScript/searchInRotatedSortedArray.js
+++ neetcode75/JavaScript/searchInRotatedSortedArray.js
@@ -33,10 +33,34 @@
 // -104 <= target <= 104
 
 var search = function(nums, target) {
+  let [l, r] = [0, nums.length - 1];
 
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    if (nums[mid] === target) return mid;
+
+    if (nums[l] <= nums[mid]) {
+      if (nums[l] <= target && target < nums[mid]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[r]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+  }
+
+  return -1;
 };
 
 let actual1 = search([4,5,6,7,0,1,2], 0) // expect 4
 console.log('Expect 0: ', actual1);
 let actual2 = search([4,5,6,7,0,1,2], 3) // expect -1
 console.log('Expect 3: ',actual2)
+
+let actual3 = search([1,3], 3) // expect 1;
+console.log('Expect 1: ', actual3);
